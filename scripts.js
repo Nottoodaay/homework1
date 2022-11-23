6f9ddd49eeca4cbfffb3d8ec108a577941f61b68
odikadzegiorgi4@gmail.com <odikadzegiorgi4@gmail.com>
2022-11-24 03:00:45 +0400

    jsHomework1

---
+++ scripts.js
@@ -0,0 +1,47 @@
+// task1
+
+
+// let a = prompt("number 1: ")
+
+// let b = prompt("number 2 : ")
+
+// console.log(Number(a)+Number(b))
+// console.log(Number(a)*Number(b))
+
+
+
+
+// task2
+
+
+// let a = prompt("number 1: ")
+
+// let b = prompt("number 2 : ")
+
+// let c = prompt("number 3 : ")
+
+// let d = prompt("number 4 : ")
+
+// if(a > b && a > c && a > d){
+//     console.log(a)
+// }if(b > a && b > c && b > d){
+//     console.log(b)
+// }if(c > a && c > b && c > d ){
+//     console.log(c)
+// }else{
+//     console.log(d)
+// }
+
+
+
+// task 3
+
+
+let a = prompt("number 1: ")
+
+if(Number(a)%2 == 0){
+    console.log("luwi")
+}else{
+    console.log("kenti")
+}
+
